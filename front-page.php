c6c18ba66982c6696a56c060234d1d017ce77c04
yuuki-fuji <yuukikun666@gmail.com>
2024-11-08 14:16:09 +0900

    add referer

--- front-page.php
+++ front-page.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 
-testです
-testです
+<h2>Demoです</h2>
+
+<?php
+// リファラ情報を取得
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '直接アクセス';
+
+echo "このページに遷移してきたURL: " . htmlspecialchars($referer, ENT_QUOTES, 'UTF-8');
+?>
+
 
 
 <?php get_footer(); ?>
